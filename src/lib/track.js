dd3c79d78c769490144108a541664712086cdc88
Emirkan Acar <nydaolian@gmail.com>
2018-06-20 18:16:44 +0300

    change data type

--- src/lib/track.js
+++ src/lib/track.js
@@ -4,49 +4,49 @@ const request = require('./request');
 
 module.exports = {
     trackAudioAnalysis: (data, callback) => {
-        if(!data.id)
+        if(!data.trackID)
         {
-            callback('id required')
+            callback('trackID required')
         }
 
         let options = {};
-        options.url = 'https://api.spotify.com/v1/audio-analysis/' + data.id;
+        options.url = 'https://api.spotify.com/v1/audio-analysis/' + data.trackID;
 
         request('GET', options, callback)
     },
 
     tracksAudioFeatures: (data, callback) => {
-        if(!data.ids)
+        if(!data.trackIDs)
         {
-            callback('ids required')
+            callback('trackIDs required')
         }
 
         let options = {};
-        options.url = 'https://api.spotify.com/v1/audio-features?ids=' + data.ids;
+        options.url = 'https://api.spotify.com/v1/audio-features?ids=' + data.trackIDs;
 
         request('GET', options, callback)
     },
 
     trackAudioFeatures: (data, callback) => {
-        if(!data.id)
+        if(!data.trackID)
         {
-            callback('id required')
+            callback('trackID required')
         }
 
         let options = {};
-        options.url = 'https://api.spotify.com/v1/audio-features/' + data.id;
+        options.url = 'https://api.spotify.com/v1/audio-features/' + data.trackID;
 
         request('GET', options, callback)
     },
 
     severalTracks: (data, callback) => {
-        if(!data.id)
+        if(!data.trackID)
         {
-            callback('id required')
+            callback('trackID required')
         }
 
         let options = {};
-        options.url = 'https://api.spotify.com/v1/tracks/' + data.id;
+        options.url = 'https://api.spotify.com/v1/tracks/' + data.trackID;
 
         request('GET', options, callback)
     }
